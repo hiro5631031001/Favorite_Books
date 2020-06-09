2407aaa3896f2b3a269714f0a17f433891b8d7f4
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-09 18:43:47 +0900

    boooks_controller searchアクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,4 +2,11 @@ class BooksController < ApplicationController
   def index
     # @book = Book.find(1)
   end
+
+  def search
+    if params[:keyword]
+      @books = RakutenWebService::Books::Book.search(title: params[:keyword])
+    end
+  end
+
 end
