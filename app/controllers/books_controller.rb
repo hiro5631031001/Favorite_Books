c048add3721d970f1c4b37c3d73c55a73ac2aa4e
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-20 01:41:10 +0900

    show,edit,updateアクション作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,9 +20,13 @@ class BooksController < ApplicationController
  end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id]) 
+    @book.update(book_params)
+    redirect_to root_path
   end
 
   def destroy
