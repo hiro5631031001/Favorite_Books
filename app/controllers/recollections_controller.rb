13d6786245abe8c4db4c957e476064e93512257d
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-21 16:02:56 +0900

    recollection_params作成

--- app/controllers/recollections_controller.rb
+++ app/controllers/recollections_controller.rb
@@ -6,6 +6,20 @@ class RecollectionsController < ApplicationController
 
   def create
     @recollection = Recollection.create(recollection_params)
+  end
+
+  private
+
+  def recollection_params
+    params.require(:impression).permit(:book_id,
+                                        :read_term,
+                                        :readed_day,
+                                        :point,
+                                        :reread_timing,
+                                        :phrase,
+                                        :note
+                                ).merge(book_id: params[:book_id])
+    end
     
   end
 
