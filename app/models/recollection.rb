ebaefeb7d10f04b6cf9d7069d9578348047420c2
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-21 15:35:50 +0900

    read_term enum設定

--- app/models/recollection.rb
+++ app/models/recollection.rb
@@ -1,3 +1,13 @@
 class Recollection < ApplicationRecord
   belongs_to :book
+
+  enum read_term: {
+    "-----": 0,
+    one_day: 1,
+    two_three_days: 2,
+    one_week: 3,
+    two_four_weeks: 4,
+    one_month_over: 5
+  }, _prefix: true
+
 end
