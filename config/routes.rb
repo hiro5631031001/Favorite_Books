3efc35ed2e1e3b7d943e34edd82682b2e78ca1ac
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-22 17:37:07 +0900

    user#show他　routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'books#index'
+  resources :users, only: [:show, :edit, :update]
   resources :books do
     resources :recollections 
   end
