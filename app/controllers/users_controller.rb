2d8a2daea6467c445af7a05fca75010ec774d42e
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-23 19:12:29 +0900

    書籍情報をuserコントローラーでビューへ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @buy_books = @user.books.where(status: "book_recollection_none").order("created_at DESC").limit(10)
+    @readed_books = @user.books.where(status: "book_recollection_exist").order("created_at DESC").limit(10)
   end
 end
