2ae3a151f11242fe27b675ce1f936e43e79aa2b3
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-24 19:39:15 +0900

    Update users_controller.rb
    set_user before_actionを設定
    edit,updateメソッドを設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  
+  
   def show
-    @user = User.find(params[:id])
     @buy_books = @user.books.where(status: "book_recollection_none").order("created_at DESC").limit(10)
     @readed_books = @user.books.where(status: "book_recollection_exist").order("created_at DESC").limit(10)
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :password, :birth_day, :image)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
