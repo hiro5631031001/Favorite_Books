894fe4a33a2f6e77d608abc70d3793cca32a8ce9
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-24 20:59:30 +0900

    bookcaseメソッドのrouting作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   get 'users/show'
   devise_for :users
   root to: 'books#index'
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :update] do
+    member do
+      get 'bookcase'
+    end
+  end
   resources :books do
     resources :recollections 
   end
