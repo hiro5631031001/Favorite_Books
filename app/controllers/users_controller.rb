0b0c7255d7d9b674d79df7d79aeb065f29163f48
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-27 01:05:38 +0900

    users#categoryメソッド作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :bookcase]
+  before_action :set_user, only: [:show, 
+                                  :edit, 
+                                  :update, 
+                                  :bookcase, 
+                                  :category]
   before_action :set_user_recollection, only: [:show]
   
   
@@ -17,6 +21,17 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def category
+    @novels = @user.books.category_novel.all.order("created_at DESC").limit(8)
+    @managements = @user.books.category_management.all.order("created_at DESC").limit(8)
+    @economies = @user.books.category_economy.all.order("created_at DESC").limit(8)
+    @philosophies = @user.books.category_philosophy.all.order("created_at DESC").limit(8)
+    @psychology = @user.books.category_psychology.all.order("created_at DESC").limit(8)
+    @motivations = @user.books.category_motivation.all.order("created_at DESC").limit(8)
+    @comics = @user.books.category_comic.all.order("created_at DESC").limit(8)
+    @etcs = @user.books.category_etc.all.order("created_at DESC").limit(8)
   end
 
   def bookcase
