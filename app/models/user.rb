e98fb7f41ec192d7ea4ba08fd91644b40db4cc3f
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-30 07:35:00 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,9 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   validates :nickname,  presence: true
-  validates :email,     presence: true
-  validates :email,     uniqueness: true
+  validates :nickname,  length: { maximum: 20, message: "は20文字以下で入力してください" }, allow_blank: true
+  validates :email,     presence: true, uniqueness: true
   validates :birth_day, presence: true
   validates :image,     presence: true
-
 
 end
