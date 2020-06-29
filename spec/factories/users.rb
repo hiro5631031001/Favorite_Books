be852ebcf54eec2db7360460639d6a0125d58771
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-30 06:39:18 +0900

    Update users.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     password = Faker::Internet.password(min_length: 6)
     nickname {Faker::Name.last_name}
     email {Faker::Internet.free_email}
-    image image {File.open("#{Rails.root}/public/images/usertest.png")}
+    image {File.open("#{Rails.root}/public/images/usertest.png")}
     birth_day {Faker::Date.birthday}
     password {password}
     password_confirmation {password}
