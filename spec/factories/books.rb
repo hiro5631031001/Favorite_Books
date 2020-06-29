f612c875312ff3e6fe8c20bdcf5090a45ba6e014
hiro5631031001 <hiro5631031001@gmail.com>
2020-06-30 06:39:12 +0900

    Update books.rb

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :book do
-    title = {Faker::Book.title}
+    title {Faker::Book.title}
     author {Faker::Book.author}
     publisher {Faker::Book.publisher}
     category {Faker::Number.between(from: 1, to: 8)}
-    status {Faker::Number.between(from: 1, to: 2)}
+    status = 0
     kindle {Faker::Number.between(from: 1, to: 2)}
     image {File.open("#{Rails.root}/public/images/booktest.jpg")}
     buy_date {Faker::Date.between(from: '2000-01-01', to: Date.today)}
